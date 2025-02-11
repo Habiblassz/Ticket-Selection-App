e681d1363d4c4e572b07d9d5eca651b158822ae0
Lawal Habib <habiblass2001@gmail.com>
2025-02-11 21:25:35 +0100

    updated the theme and style of attendeeDetails component according to the figma design

--- ticket-generator/src/AttendeeDetails.jsx
+++ ticket-generator/src/AttendeeDetails.jsx
@@ -1,4 +1,3 @@
-// AttendeeDetails.jsx
 import React, { useState } from "react";
 import "./AttendeeDetails.css";
 
@@ -53,25 +52,41 @@ const AttendeeDetails = ({ onBack, onSubmit, ticketQuantity }) => {
 				</div>
 
 				<div className="upload-area">
-					<label htmlFor="avatar-upload" className="upload-label">
-						<div className="upload-icon">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="30"
-								height="30"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="#ddd"
-								strokeWidth="2"
-								strokeLinecap="round"
-								strokeLinejoin="round">
-								<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-								<polyline points="17 9 12 4 7 9"></polyline>
-								<line x1="12" y1="4" x2="12" y2="16"></line>
-							</svg>
+					<p>Upload Profile Photo</p>
+					<div className="inner-box">
+						<div className="inner-smaller-box">
+							<label htmlFor="avatar-upload" className="upload-label">
+								<div className="upload-icon">
+									{/* <svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="30"
+									height="30"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="#ddd"
+									strokeWidth="2"
+									strokeLinecap="round"
+									strokeLinejoin="round">
+									<path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+									<polyline points="17 9 12 4 7 9"></polyline>
+									<line x1="12" y1="4" x2="12" y2="16"></line>
+									</svg> */}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										viewBox="0 0 16 16">
+										<path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383" />
+										<path d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708z" />
+									</svg>
+								</div>
+								<div className="upload-text">
+									Drag & drop or click to upload
+								</div>
+							</label>
 						</div>
-						<div className="upload-text">Drag & drop or click to upload</div>
-					</label>
+					</div>
 					<input
 						type="file"
 						id="avatar-upload"
@@ -84,8 +99,8 @@ const AttendeeDetails = ({ onBack, onSubmit, ticketQuantity }) => {
 							<img src={URL.createObjectURL(avatar)} alt="Avatar Preview" />
 						</div>
 					)}
-					{errors.avatar && <p className="error">{errors.avatar}</p>}
 				</div>
+				{errors.avatar && <p className="error">{errors.avatar}</p>}
 
 				<div className="form-group">
 					<label htmlFor="name">Enter your name</label>
@@ -99,22 +114,24 @@ const AttendeeDetails = ({ onBack, onSubmit, ticketQuantity }) => {
 				</div>
 
 				<div className="form-group">
-					<label htmlFor="email">Enter your email *</label>
+					<label htmlFor="email">Enter your email </label>
 					<input
 						type="email"
 						id="email"
 						value={email}
+						placeholder="✉️  hello@avioflagos.io"
 						onChange={(e) => setEmail(e.target.value)}
 					/>
 					{errors.email && <p className="error">{errors.email}</p>}
 				</div>
 
 				<div className="form-group">
-					<label htmlFor="project">About the project</label>
+					<label htmlFor="project">Special request?</label>
 					<textarea
 						id="project"
 						className="project-textarea"
 						value={project}
+						placeholder="Textarea"
 						onChange={(e) => setProject(e.target.value)}></textarea>
 				</div>
 
